c71076648dc387fbd11c0e828e779c942b8d29c0
ChrisLevano <chrislevano@gmail.com>
2021-05-25 18:19:35 -0400

    dummy commit

--- src/main/java/models/TransactionValues.java
+++ src/main/java/models/TransactionValues.java
@@ -1,2 +1,108 @@
-package models;public class TransactionValuez {
+package models;
+
+import annotations.Column;
+import annotations.Connection;
+import annotations.Entity;
+import annotations.ForeignKey;
+import annotations.Id;
+import annotations.Setter;
+import annotations.Table;
+
+@Entity
+@Connection(
+        url = "jdbc:postgresql://project0-accounts.comfkmj3hfze.us-west-1.rds.amazonaws.com:5432/postgres?currentSchema=public",
+        username = "thomas",
+        password = "revature"
+)
+@Table(
+        name = "transactions"
+)
+public class TransactionValues {
+    @Id(
+            name = "trans_id"
+    )
+    @Column(
+            name = "trans_id",
+            type = "serial",
+            nullable = false,
+            unique = true,
+            updateable = false
+    )
+    private static int trans_id;
+    @ForeignKey(
+            name = "account_id",
+            references = "accounts"
+    )
+    @Column(
+            name = "account_id",
+            type = "int",
+            nullable = false,
+            unique = false,
+            updateable = false
+    )
+    private static int account_id;
+    @Column(
+            name = "prev_bal",
+            type = "double",
+            nullable = false,
+            unique = false,
+            length = "12,2",
+            updateable = false
+    )
+    private static double prev_bal;
+    @Column(
+            name = "change",
+            type = "double",
+            nullable = false,
+            unique = false,
+            length = "12,2",
+            updateable = false
+    )
+    private static double change;
+    @Column(
+            name = "timestamp",
+            type = "timestamp",
+            nullable = false,
+            unique = false,
+            updateable = false
+    )
+    private static String timestamp;
+
+    public TransactionValues() {
+    }
+
+    @Setter(
+            name = "trans_id"
+    )
+    public static void setTrans_id(int trans_id) {
+        TransactionValues.trans_id = trans_id;
+    }
+
+    @Setter(
+            name = "account_id"
+    )
+    public static void setAccount_id(int account_id) {
+        TransactionValues.account_id = account_id;
+    }
+
+    @Setter(
+            name = "prev_bal"
+    )
+    public static void setPrev_bal(double prev_bal) {
+        TransactionValues.prev_bal = prev_bal;
+    }
+
+    @Setter(
+            name = "change"
+    )
+    public static void setChange(double change) {
+        TransactionValues.change = change;
+    }
+
+    @Setter(
+            name = "timestamp"
+    )
+    public static void setTimestamp(String timestamp) {
+        TransactionValues.timestamp = timestamp;
+    }
 }
--- src/main/java/models/UserAccount.java
+++ src/main/java/models/UserAccount.java
@@ -1,2 +1,76 @@
-package models;public class UserAccount {
+package models;
+
+import annotations.Column;
+import annotations.Entity;
+import annotations.ForeignKey;
+import annotations.Id;
+import annotations.Table;
+
+@Entity
+@Table(
+        name = "accounts"
+)
+public class UserAccount {
+    @Id(
+            name = "account_num"
+    )
+    @Column(
+            name = "account_num",
+            nullable = false,
+            unique = true,
+            type = "serial",
+            updateable = false
+    )
+    private static int account_num;
+    @ForeignKey(
+            name = "user_id",
+            references = "users"
+    )
+    @Column(
+            name = "user_id",
+            nullable = false,
+            unique = true,
+            type = "int",
+            updateable = false
+    )
+    private static int id;
+    @Column(
+            name = "balance",
+            nullable = false,
+            unique = false,
+            type = "double",
+            length = "12,2",
+            updateable = true
+    )
+    private double balance;
+
+    public static int getAccount_num() {
+        return account_num;
+    }
+
+    public static void setAccount_num(int account_num) {
+        models.UserAccount.account_num = account_num;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        models.UserAccount.id = id;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public UserAccount(int id, double balance) {
+        System.out.println("Registering user...");
+        models.UserAccount.id = id;
+        this.balance = balance;
+    }
 }
